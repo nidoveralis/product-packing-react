bb9bbe8025a7c2f56a9bb44b21c706b4f130407a
Nidoveralis <nidoveralis@gmail.com>
2023-06-10 23:29:55 +0400

    fix: corrected semantic errors

--- src/components/Card/Cards.js
+++ src/components/Card/Cards.js
@@ -3,11 +3,14 @@ import './Card.css';
 
 
 function Card({item, onScanCard}) {
-
+//width/count*scan
+//style{{backgroundColor 0 - 25 -100%}}
+// background: linear-gradient(to left,  rgba(243, 240, 233, 0.5) `${Math.floor(item.scan*100/item.count)}%`, rgba(42, 173, 46, 0.25) `${Math.floor(100 - (item.scan*100/item.count))}%`);
   const [cardNumber, setCardNumber] = React.useState(item.count>1 ? "Развернуть" : item.barcode)
 
   const tagCardClass = `card__tag ${item.tag ? "card__tag_active" : ""}`;
-  const countCardClass = `card__count conteiner ${item.scan ? "card__count_active" : ""}`;///////card__count_no-one 
+  const countContent = item.count===1 || item.scan===0 ? `${item.count} шт.` : `${item.scan} из ${item.count} шт.`;
+  const countCardClass = `card__count conteiner ${item.full ? "card__count_active" : ""}`;///////card__count_no-one 
   const numberCardClass = `card__number ${item.count>1 ? "card__button-more" : ""}`;
 
 
@@ -15,17 +18,17 @@ function Card({item, onScanCard}) {
     
     onScanCard(item)
   };
-  console.log(item.scan)
+
   return (
-    <div className="card" >
+    <li className="card" >
       <div className="card__info">
         <div className="card__img" style={{backgroundImage: `url(${item.img})`}} />
         <p className="card__description">{item.description}</p>
-        <button className={tagCardClass}>{item.tag}</button>
+        <p className={tagCardClass}>{item.tag}</p>
       </div>
-      <button className={countCardClass} onClick={handleScanProduct} >{`${item.count} шт.`}</button>
-      <button className={numberCardClass}>{cardNumber}</button>              
-      </div>
+      <div className={countCardClass} style={item.full ? {backgroundColor:'#2AAD2E'} : { background: `linear-gradient(to left, rgba(243, 240, 233, 0.5) ${Math.floor(100 - (item.scan*100/item.count))}%, rgba(42, 173, 46, 0.25) ${Math.floor(item.scan*100/item.count)}%)`}} >{countContent}</div>
+      <button className={numberCardClass} onClick={handleScanProduct} >{cardNumber}</button>              
+      </li>
   )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,22 @@
 import logoYandex from '../../images/logo-yandex.jpg';
 import logoMarket from '../../images/logo-market.jpg';
-import './NavbarMenu.css';
+import './Header.css';
 
-function NavbarMenu() {
+function Header() {
   return(
-    <div className="navbar">
-      <div className="navbar-menu">
+    <header className="header">
+      <div className="header-menu">
         <div className="navbar-menu__burger">
           <span className="navbar-menu__item"></span>
         </div>
-        <div className="navbar-logo">
-          <img src={logoYandex} alt="Логотип Яндекс" className="navbar-logo__img" />
-          <img src={logoMarket} alt="Логотип Яндекс Маркет" className="navbar-logo__img" />
-          <p className="navbar-logo__title">Склад</p>
+        <div className="header-logo">
+          <img src={logoYandex} alt="Логотип Яндекс" className="header-logo__img" />
+          <img src={logoMarket} alt="Логотип Яндекс Маркет" className="header-logo__img" />
+          <p className="header-logo__title">Склад</p>
         </div>
       </div>
-      <h3 className="navbar__title">Упаковка</h3>
+
+      <h3 className="header__title">Упаковка</h3>
       
       <div className="user-menu">
         <div className="user-menu__info">
@@ -27,8 +28,8 @@ function NavbarMenu() {
         </div>
         <button className="user-menu__setting"></button>
       </div>
-    </div>
+    </header>
   )
 }
 
-export default NavbarMenu;
+export default Header;
