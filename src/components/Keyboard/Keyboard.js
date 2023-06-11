99155abe095c4f16f88e6bbb965159fd78e99a4b
MariaMantusova <maria.mantusova@mail.ru>
2023-06-11 18:29:06 +0300

    feat: add packing page

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Keyboard.css';
 import iconKeyboard from "../../images/icon__keyboard.svg";
 
-function Keyboard() {
+function Keyboard(props) {
     return (
         <button className="keyboard-button">
+            {props.button}
             <div className="text-container">
                 <img className="keyboard-button-text__icon" src={iconKeyboard} alt="Иконка клавиатуры"/>
                 <h2 className="keyboard-button__text">Ввести с клавиатуры</h2>
---
+++ src/components/PackingPage/PackingPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./PackingPage.css";
+import Header from "../Header/Header";
+import Keyboard from "../Keyboard/Keyboard";
+import FooterMy from "../FooterMy/FooterMy";
+import Button from "../Button/Button";
+import iconBox from "../../images/icon__box.svg";
+
+function PackingPage(props) {
+    return (
+        <>
+            <Header/>
+            <section className="packing-block">
+                <Button buttonText="Есть проблема"/>
+                <div className="packing-container">
+                    <img className="packing__image" src={iconBox} alt="картинка коробки"/>
+                    <h1 className="packing__text">Упакуйте товары и&nbsp;сканируйте коробку <span className="packing-type">{props.type}</span></h1>
+                </div>
+            </section>
+            <Keyboard button={<button className="button__back">Назад</button>}/>
+            <FooterMy/>
+        </>
+    )
+}
+
+export default PackingPage;
