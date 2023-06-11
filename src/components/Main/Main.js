3bb93e5b6a76d6a0413e92e608ffbbf4242b87e0
Nidoveralis <nidoveralis@gmail.com>
2023-06-11 17:49:13 +0400

    feat: added component Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,19 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Products from '../Products/Products';
+
+function Main(props){
+  return(
+    <>
+        <Header />
+        <main className="content">
+          <button className="content__button have-problem">Есть проблема</button>
+          <Products cards={props.cards} onScanCard={props.onScanCard} />
+            <button className="content__button close-box">Закрыть коробку</button>
+        </main>
+        <Footer />
+    </>
+  )
+};
+
+export default Main;
