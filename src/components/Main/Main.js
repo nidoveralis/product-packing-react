4113575a1754e54c6e93e0319282be825e705397
MariaMantusova <maria.mantusova@mail.ru>
2023-06-13 23:32:50 +0300

    feat: add changing on main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import Products from "../Products/Products";
 import Button from "../Button/Button";
 import Keyboard from "../Keyboard/Keyboard";
 import Footer from "../Footer/Footer";
+import editIcon from "../../images/icon__edit.svg";
 
 function Main(props){
   return(
@@ -13,7 +14,12 @@ function Main(props){
           <Products cards={props.cards} onScanCard={props.onScanCard} />
           <Button buttonText="Закрыть коробку"/>
         </main>
-        <Keyboard/>
+        <Keyboard>
+            <div className="edit__container">
+                <img className="edit__icon" src={editIcon} alt="Иконка изменить состав"/>
+                <p className="edit__text">Изменить состав</p>
+            </div>
+        </Keyboard>
         <Footer/>
     </>
   )
